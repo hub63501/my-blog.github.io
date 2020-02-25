06523f67b62cf8a89538a3d2d67b5de06fcb061b
hub63501 <geo19.gh@outlook.com>
2020-02-25 10:19:51 +0000

    test

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/Projects/my-blog.github.io/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/Projects/my-blog.github.io/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/Projects/my-blog.github.io/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/Projects/my-blog.github.io/src/pages/page-2.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/my-blog.github.io/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/my-blog.github.io/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/home/geo-fedora-lenovo/Documents/my-blog.github.io/src/pages/page-2.js")))
 }
 
